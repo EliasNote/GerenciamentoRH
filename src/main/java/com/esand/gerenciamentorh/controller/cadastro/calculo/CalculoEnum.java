a2b8b40a87e5bdd2a13617d15239a7c2695164f4
Elias Sand <elias.coder1@gmail.com>
2025-01-21 17:37:14 -0300

    feat

--- src/main/java/com/esand/gerenciamentorh/controller/cadastro/calculo/CalculoEnum.java
+++ src/main/java/com/esand/gerenciamentorh/controller/cadastro/calculo/CalculoEnum.java
@@ -3,17 +3,15 @@ package com.esand.gerenciamentorh.controller.cadastro.calculo;
 import com.esand.gerenciamentorh.controller.cadastro.calculo.impostos.Fgts;
 import com.esand.gerenciamentorh.controller.cadastro.calculo.impostos.Inss;
 import com.esand.gerenciamentorh.controller.cadastro.calculo.impostos.Irpf;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum CalculoEnum {
     INSS(new Inss()),
     IRPF(new Irpf()),
     FGTS(new Fgts());
 
     public final CalculoStrategy strategy;
-
-    CalculoEnum(CalculoStrategy calculoStrategy) {
-        this.strategy = calculoStrategy;
-    }
 }
